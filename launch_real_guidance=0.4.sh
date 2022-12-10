39ffdadd4937b421dd537bd8b937e7dcdaa6c33c
Brandon Trabucco <btrabucc@matrix.ml.cmu.edu>
2022-12-10 17:49:00 -0500

    updating experiment scripts for the matrix

---
+++ launch_real_guidance=0.4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=spurge
+#SBATCH --exclude=matrix-1-12,matrix-0-24,matrix-1-16
+#SBATCH --time=72:00:00
+#SBATCH --nodes=1
+#SBATCH --partition=russ_reserved
+#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-gpu=8
+#SBATCH --mem=128g
+ 
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate semantic-aug
+cd ~/spurge/semantic-aug
+
+torchrun --standalone --nnodes 1 --nproc_per_node 4 \
+example.py --logdir ~/spurge/semantic-aug/baselines/real-guidance-0.4 \
+--strength 0.4 --num-synthetic 20 --synthetic-probability 0.5 --num-trials 8
