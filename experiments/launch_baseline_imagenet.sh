4593dadc534cd5314aaf6f9a073598206fa17f44
Brandon Trabucco <brandon@btrabucco.com>
2022-12-12 01:35:52 -0500

    adding massive parallelism for imagenet jobs

---
+++ experiments/launch_baseline_imagenet.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=spurge
+#SBATCH --exclude=matrix-1-12,matrix-0-24,matrix-1-16,matrix-1-4,matrix-2-13,matrix-1-8
+#SBATCH --time=72:00:00
+#SBATCH --nodes=1
+#SBATCH --partition=russ_reserved
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-gpu=8
+#SBATCH --mem=32g
+#SBATCH --array=0-47
+ 
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate semantic-aug
+cd ~/spurge/semantic-aug
+
+torchrun --standalone --nnodes 48 --node_rank $SLURM_ARRAY_TASK_ID --nproc_per_node 1 \
+train_classifier.py --logdir ./imagenet-baselines/baseline \
+--dataset imagenet --aug none \
+--strength 0.0 --num-synthetic 0 \
+--synthetic-probability 0.0 --num-trials 8
---
+++ experiments/real_guidance/launch_real_guidance=0.5_imagenet.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=spurge
+#SBATCH --exclude=matrix-1-12,matrix-0-24,matrix-1-16,matrix-1-4,matrix-2-13,matrix-1-8
+#SBATCH --time=72:00:00
+#SBATCH --nodes=1
+#SBATCH --partition=russ_reserved
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-gpu=8
+#SBATCH --mem=32g
+#SBATCH --array=0-47
+ 
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate semantic-aug
+cd ~/spurge/semantic-aug
+
+torchrun --standalone --nnodes 48 --node_rank $SLURM_ARRAY_TASK_ID --nproc_per_node 1 \
+train_classifier.py --logdir ./imagenet-baselines/real-guidance-0.5 \
+--dataset imagenet --aug real-guidance \
+--strength 0.5 --num-synthetic 20 \
+--synthetic-probability 0.5 --num-trials 8
