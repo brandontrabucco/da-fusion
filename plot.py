e343e83005a42c3a426e5988d06301f0fa5dc84e
Brandon Trabucco <brandon@btrabucco.com>
2022-12-17 16:50:50 -0500

    format plotting script

--- plot.py
+++ plot.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib
 import pandas as pd
 import seaborn as sns
 
@@ -7,60 +8,140 @@ import glob
 import argparse
 
 
+def pretty(text):
+    """Convert a string into a consistent format for
+    presentation in a matplotlib pyplot:
+    this version looks like: One Two Three Four
+    """
+
+    text = text.replace("_", " ")
+    text = text.replace("-", " ")
+    text = text.replace("/", " ")
+    text = text.strip()
+    prev_c = None
+    out_str = []
+    for c in text:
+        if prev_c is not None and \
+                prev_c.islower() and c.isupper():
+            out_str.append(" ")
+            prev_c = " "
+        if prev_c is None or prev_c == " ":
+            c = c.upper()
+        out_str.append(c)
+        prev_c = c
+    return "".join(out_str)
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser("Few-Shot Baseline")
-    parser.add_argument("--logdir", type=str, default="./baselines")
+
+    parser.add_argument("--logdirs", nargs="+", type=str, default=[
+        "./spurge-baselines", "./coco-baselines", "./imagenet-baselines"])
+    
+    parser.add_argument("--datasets", nargs="+", type=str, 
+                        default=["Spurge", "COCO", "ImageNet"])
+    
+    parser.add_argument("--method-names", nargs="+", type=str, 
+                        default=["baseline", "real-guidance-0.5", "textual-inversion-0.5"])
+
     args = parser.parse_args()
 
     combined_dataframe = []
 
-    for bname in os.listdir(args.logdir):
+    for dataset, logdir in zip(
+            args.datasets, args.logdirs):
 
-        bpath = os.path.join(args.logdir, bname)
+        for bname in os.listdir(logdir):
 
-        if not os.path.isdir(bpath):
-            continue
+            bpath = os.path.join(logdir, bname)
 
-        files = list(glob.glob(os.path.join(bpath, "*.csv")))
+            if not os.path.isdir(bpath):
+                continue
 
-        if len(files) == 0:
-            continue
+            files = list(glob.glob(os.path.join(bpath, "*.csv")))
+            data = pd.concat([pd.read_csv(x, index_col=0) 
+                            for x in files], ignore_index=True)
 
-        data = pd.concat([pd.read_csv(x, index_col=0) 
-                          for x in files], ignore_index=True)
+            data = data[(data["metric"] == "Accuracy") & 
+                        (data[ "split"] == "Validation")]
 
-        data = data[(data["metric"] == "Accuracy") & 
-                    (data[ "split"] == "Validation")]
+            def select_by_epoch(df):
+                selected_row = df.loc[df["value"].idxmax()]
+                return data[(data["epoch"] == selected_row["epoch"]) & 
+                            (data[ "examples_per_class"] == 
+                            selected_row["examples_per_class"])]
 
-        def select_by_epoch(df):
-            selected_row = df.loc[df["value"].idxmax()]
-            return data[(data["epoch"] == selected_row["epoch"]) & 
-                        (data[ "examples_per_class"] == 
-                         selected_row["examples_per_class"])]
+            best = data.groupby(["examples_per_class", "epoch"])
+            best = best["value"].mean().to_frame('value').reset_index()
+            best = best.groupby("examples_per_class").apply(
+                select_by_epoch
+            )
 
-        best = data.groupby(["examples_per_class", "epoch"])
-        best = best["value"].mean().to_frame('value').reset_index()
-        best = best.groupby("examples_per_class").apply(
-            select_by_epoch
-        )
+            best["method"] = bname
+            best["dataset"] = dataset
+            combined_dataframe.append(best)
 
-        best["method"] = bname
-        combined_dataframe.append(best)
+    matplotlib.rc('font', family='Times New Roman', serif='cm10')
+    matplotlib.rc('mathtext', fontset='cm')
+    plt.rcParams['text.usetex'] = False
 
     combined_dataframe = pd.concat(
         combined_dataframe, ignore_index=True)
 
-    plt.figure(figsize=(8, 4))
-    fig = sns.lineplot(x="examples_per_class", 
-                       y="value", hue="method", 
-                       data=combined_dataframe)
+    combined_dataframe = pd.concat([combined_dataframe[
+        combined_dataframe['method'] == n] for n in args.method_names])
+    
+    color_palette = sns.color_palette(n_colors=len(args.method_names))
 
-    sns.move_legend(fig, "center left", bbox_to_anchor=(1.05, 0.45))
+    fig, axs = plt.subplots(1, len(args.datasets),
+                            figsize=(8 * len(args.datasets), 7))
 
-    plt.ylabel("Accuracy (Val)")
-    plt.xlabel("Examples Per Class")
+    for i, dataset in enumerate(args.datasets):
 
-    plt.tight_layout()
-    plt.savefig(os.path.join(
-        args.logdir, f"barplot.png"))
+        results = combined_dataframe[combined_dataframe["dataset"] == dataset]
+
+        axis = sns.lineplot(x="examples_per_class", y="value", hue="method", 
+                            data=results, errorbar=('ci', 68),
+                            linewidth=4, palette=color_palette,
+                            ax=axs[i] if len(args.datasets) > 1 else axs)
+
+        if i == 0: handles, labels = axis.get_legend_handles_labels()
+        axis.legend([],[], frameon=False)
+
+        axis.set(xlabel=None)
+        axis.set(ylabel=None)
+
+        axis.spines['right'].set_visible(False)
+        axis.spines['top'].set_visible(False)
+
+        axis.xaxis.set_ticks_position('bottom')
+        axis.yaxis.set_ticks_position('left')
+
+        axis.yaxis.set_tick_params(labelsize=16)
+        axis.xaxis.set_tick_params(labelsize=16)
+
+        axis.set_xlabel("Examples Per Class", fontsize=24,
+                        fontweight='bold', labelpad=12)
+
+        axis.set_ylabel("Accuracy (Val)", fontsize=24,
+                        fontweight='bold', labelpad=12)
+
+        axis.set_title(f"dataset = {dataset}",
+                       fontsize=24, fontweight='bold', pad=12)
+
+        axis.grid(color='grey', linestyle='dotted', linewidth=2)
+
+    legend = fig.legend(handles, [pretty(x) for x in args.method_names],
+                        loc="lower center", ncol=len(args.method_names),
+                        prop={'size': 24, 'weight': 'bold'})
+
+    for i, legend_object in enumerate(legend.legendHandles):
+        legend_object.set_linewidth(4.0)
+        legend_object.set_color(color_palette[i])
+
+    plt.tight_layout(pad=1.0)
+    fig.subplots_adjust(bottom=0.25)
+
+    plt.savefig("visualization.pdf")
+    plt.savefig("visualization.png")
