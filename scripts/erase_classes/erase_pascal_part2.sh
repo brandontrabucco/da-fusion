7791fdff3ba39aa8f3ef1364153bc71f2bd7a199
Brandon Trabucco <brandon@btrabucco.com>
2023-03-25 01:17:19 -0400

    adding erasing scripts for pascal task

---
+++ scripts/erase_classes/erase_pascal_part2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=erase
+#SBATCH --exclude=matrix-1-12,matrix-0-24,matrix-1-4,matrix-2-13,matrix-1-8
+#SBATCH --time=72:00:00
+#SBATCH --nodes=1
+#SBATCH --partition=russ_reserved
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-gpu=8
+#SBATCH --mem=32g
+#SBATCH --array=0-39
+ 
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate ldm
+cd ~/spurge/semantic-aug/stable-diffusion
+
+for CLASS_NAME in 'dining table' 'dog' 'horse' 'motorcycle' 'person'; do
+
+python train-scripts/train-esd.py --prompt "$CLASS_NAME" --train_method 'full' --devices '0,0'
+
+done
