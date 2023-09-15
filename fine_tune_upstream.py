4b4759260bf68ecc99c68dc20db93e680f6743f0
Kyle Doherty <kdoherty@matrix.ml.cmu.edu>
2023-09-15 00:22:13 -0400

    fixing indent

--- fine_tune_upstream.py
+++ fine_tune_upstream.py
@@ -1083,7 +1083,6 @@ if __name__ == "__main__":
         main(args)
 
         shutil.rmtree(args.train_data_dir)
-	for filename in os.listdir(args.output_dir):
+        for filename in os.listdir(args.output_dir):
             if filename != 'learned_embeds.bin':
                 os.remove(filename)
-
