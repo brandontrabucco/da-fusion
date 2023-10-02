764f87a11689f58e495c23949efade5a18e11550
Kyle Doherty <kdoherty@matrix.ml.cmu.edu>
2023-10-02 13:51:57 -0400

    changing token dir

--- scripts/fine_tuning/fine_tune_spurge.sh
+++ scripts/fine_tuning/fine_tune_spurge.sh
@@ -14,10 +14,11 @@ conda activate semantic-aug
 cd ~/spurge/semantic-aug
 
 RANK=$SLURM_ARRAY_TASK_ID WORLD_SIZE=$SLURM_ARRAY_TASK_COUNT \
-python fine_tune.py --dataset=spurge --output_dir=./ \
+python fine_tune_upstream.py --dataset=spurge --output_dir=fine_tune_mosaic/ \
+--initializer_token "view" \
 --pretrained_model_name_or_path="CompVis/stable-diffusion-v1-4" \
 --resolution=512 --train_batch_size=4 --lr_warmup_steps=0 \
 --gradient_accumulation_steps=1 --max_train_steps=1000 \
 --learning_rate=5.0e-04 --scale_lr --lr_scheduler="constant" \
 --mixed_precision=fp16 --revision=fp16 --gradient_checkpointing \
---only_save_embeds --num-trials 8 --examples-per-class 1 2 4 8 16 
+--only_save_embeds --num-trials 8 --examples-per-class 1 2 4 8 16
